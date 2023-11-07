93eddb879446743b065e0cd7f8dcb2d84de393fc
vericamitreva <mitrevav98@gmail.com>
2023-11-07 16:23:51 +0100

    Header and Home Component made Responsive

--- space-tourism-app/src/components/Header/Header.js
+++ space-tourism-app/src/components/Header/Header.js
@@ -1,25 +1,33 @@
-import './header.css'
-import logo from './../assets/shared/logo.svg'
+import './header.css';
+import logo from './../assets/shared/logo.svg';
 import { NavLink } from 'react-router-dom';
-
 
 export default function Header() {
     return (
         <header>
-            <div className="site-logo"><img src={logo} alt="Logo" /></div>
-            <svg width="33%" height="1" viewBox="0 0 100% 1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
-                <rect opacity="0.251485" width="100%" height="1" fill="white" />
+            <svg className="site-logo-svg" width="100%" height="1" viewBox="0 0 100% 1" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg">
+                <rect opacity="0.251485" width="35%" height="1" fill="white" />
             </svg>
-            <nav className="navbar">
-                <ul className="menu">
-                    <li><NavLink to="/home" activeClassName="active"><span className='navbar-numbers'>00</span> HOME</NavLink></li>
-                    <li><NavLink to="/destination" activeClassName="active"><span className='navbar-numbers'>01</span> DESTINATION</NavLink></li>
-                    <li><NavLink to="/crew" activeClassName="active"><span className='navbar-numbers'>02</span> CREW</NavLink></li>
-                    <li><NavLink to="/technology" activeClassName="active"><span className='navbar-numbers'>03</span> TECHNOLOGY</NavLink></li>
-                </ul>
-            </nav>
+            <div className='header-container'>
+                <div className="site-logo"><img src={logo} alt="Logo" /></div>
+                <div className="menu-container">
+                    <button className='menu-icon'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21" viewBox="0 0 24 21" fill="none">
+                            <rect width="24" height="3" fill="#D0D6F9" />
+                            <rect y="9" width="24" height="3" fill="#D0D6F9" />
+                            <rect y="18" width="24" height="3" fill="#D0D6F9" />
+                        </svg>
+                    </button>
+                </div>
+                <nav className="navbar">
+                    <ul className="menu">
+                        <li><NavLink to="/home" activeClassName="active"><span className='navbar-numbers'>00</span> HOME</NavLink></li>
+                        <li><NavLink to="/destination" activeClassName="active"><span className='navbar-numbers'>01</span> DESTINATION</NavLink></li>
+                        <li><NavLink to="/crew" activeClassName="active"><span className='navbar-numbers'>02</span> CREW</NavLink></li>
+                        <li><NavLink to="/technology" activeClassName="active"><span className='navbar-numbers'>03</span> TECHNOLOGY</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
-
-
